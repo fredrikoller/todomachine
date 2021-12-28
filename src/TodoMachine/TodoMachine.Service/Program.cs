3f9763fa1c3135b56f0c4add416b40be90c0d7f6
Fredrik Öller <oller.fredrik@gmail.com>
2021-12-28 15:48:30 +0100

    adding rabbitmq to service

--- src/TodoMachine/TodoMachine.Service/Program.cs
+++ src/TodoMachine/TodoMachine.Service/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MassTransit;
+using MassTransit.Definition;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using TodoMachine.Components.Consumers;
+using TodoMachine.Services;
+
+var isService = !(Debugger.IsAttached || args.Contains("--console"));
+
+var builder = new HostBuilder()
+    .ConfigureAppConfiguration((hostingContext, config) => 
+    {
+        config.AddJsonFile("appsettings.json", true);
+        config.AddEnvironmentVariables();
+        
+        if (args != null)
+        {
+            config.AddCommandLine(args);
+        }
+    })
+    .ConfigureServices((hostContext, services) => 
+    {
+        services.TryAddSingleton(KebabCaseEndpointNameFormatter.Instance);
+        services.AddMassTransit(cfg => 
+        {
+            cfg.AddConsumersFromNamespaceContaining<SubmitTodoConsumer>();
+            cfg.AddBus(ConfigureBus);
+        });
+
+        services.AddHostedService<MassTransitConsoleHostedService>();
+    })
+    .ConfigureLogging((hostContext, logging) => 
+    {
+        logging.AddConfiguration(hostContext.Configuration.GetSection("Logging"));
+        logging.AddConsole();
+    });
+
+
+await builder.RunConsoleAsync();
+//builder.Build();
+
+static IBusControl ConfigureBus(IServiceProvider provider)
+{
+    return Bus.Factory.CreateUsingRabbitMq(cfg => 
+    {
+        cfg.ConfigureEndpoints((IBusRegistrationContext)provider);
+    });
+}
