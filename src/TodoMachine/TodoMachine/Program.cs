8610a588b33529485462afbc99716c5091a0cd47
Fredrik Öller <oller.fredrik@gmail.com>
2021-12-28 19:35:08 +0100

    added rabbitmq to api project

--- src/TodoMachine/TodoMachine/Program.cs
+++ src/TodoMachine/TodoMachine/Program.cs
@@ -1,26 +1,25 @@
 using MassTransit;
+using MassTransit.Definition;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
-using TodoMachine.Components;
-using TodoMachine.Components.Consumers;
 using TodoMachine.Contracts;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.TryAddSingleton(KebabCaseEndpointNameFormatter.Instance);
 builder.Services.AddMassTransit(config =>
 {
-    config.AddConsumer<SubmitTodoConsumer>();
     config.AddBus(provider => Bus.Factory.CreateUsingRabbitMq());
+    
     config.AddRequestClient<ISubmitTodo>();
 });
-
+builder.Services.AddMassTransitHostedService();
 
 var app = builder.Build();
 
